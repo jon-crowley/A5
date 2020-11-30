0aa489bffc6143aabbdc0b3d9fad28f9868b14b4
Jonathan J Crowley <jonathanjcrowley@Jonathans-MacBook-Pro.local>
2020-11-29 20:40:04 -0500

    dao update

--- daos/questions-dao.js
+++ daos/questions-dao.js
@@ -1,5 +1,5 @@
 const questionsModel = require('../models/questions/questions-model.js')
 const findAllQuestions = () => questionsModel.find()
 const findQuestionById = (qId) => questionsModel.findById(qId)
-const findQuestionsForQuiz = (qzid) => questionsModel.findById(qzid)
+const findQuestionsForQuiz = (qzid) => questionsModel.find({quizId: qzid})
 module.exports = { findAllQuestions, findQuestionById, findQuestionsForQuiz }
